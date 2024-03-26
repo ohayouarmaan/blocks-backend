695db6926775437d70aa0f0ab49fe70b5a394fd4
Armaan Gupta <ohayouarmaan@gmail.com>
2024-03-26 21:55:32 +0530

    Removed node_modules

--- dist/router/Blog.router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Blog_controller_1 = require("../controller/Blog.controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/", Blog_controller_1.test);
-exports.default = router;
